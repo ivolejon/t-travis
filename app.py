7b33f6a063740a9c24571e829f8440d5de550e55
ivo.lejon@gmail.com <ivo.lejon@gmail.com>
2017-07-18 23:10:44 +0100

    db redis

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 # coding: utf-8
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLALCHEMY_DATABASE_URI']
+
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello_world():
--- tests/test1.py
+++ tests/test1.py
@@ -1,21 +1,36 @@
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
+import redis
 import unittest
 
-from app import app
+from app import app, db
+from models import *
 
 class TestCase(unittest.TestCase):
     def setUp(self):
         app.config['TESTING'] = True
         self.app = app.test_client()
+        db.create_all()
 
     def tearDown(self):
-        pass
+        db.session.remove() 
+        db.drop_all()
 
     def test_index(self):
         res = self.app.get('/')
         assert 'World' in res.data
+    
+    def test_redis(self):
+        rs = redis.from_url(os.environ['REDIS_URL'])
+        rs.set('foo', 'bar')
+        value = rs.get('foo')
+        assert 'bar' in value
+
+    def test_pg(self):
+        new_user = User(email="admin@gmail.com", role='admin')
+        db.session.add(new_user)
+        db.session.commit()
+            
     
 if __name__ == '__main__':
     unittest.main()
